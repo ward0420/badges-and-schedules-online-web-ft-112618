ef717a11551dd42589aed4f7c9abb4389e17ea2b
Don Ward <10is0420@gmail.com>
2018-11-29 03:56:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+  
+
+def batch_badge_creator(badges)
+  attendees = []
+  assign_rooms = 1
+  badges.each do |attendees|
+    puts "Hello, #{names}! You'll be assigned to room #{assign_rooms}!"
+  assign_rooms += 1
+end
+  return "Hello, #{names}! You'll be assigned to room #{assign_rooms}!"
+end
+
+#brothers = ["Tim", "Tom", "Jim"]
+#counter = 1
+#brothers.each do |brother|
+  #puts "This is loop number #{counter}"
+  #puts "Stop hitting yourself #{brother}!"
+  #counter += 1
+#end
