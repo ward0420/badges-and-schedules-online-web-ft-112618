c6e041160a26413f5005d662dc8cf6c94fc452c2
Don Ward <10is0420@gmail.com>
2018-12-01 15:15:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+  
+
+def batch_badge_creator (attendees)
+  attendees.each do |badges|
+    puts badges
+end
+    return badges
+end
+
+def assign_rooms(attendees)
+attendees.each do |attendees|
+  puts "Hello, #{attendees}"
+end
+return "Hello, #{attendees}"
+room_assignments += 1
+end
+
+
+#thers = ["Tim", "Tom", "Jim"]
+#counter = 1
+#brothers.each do |brother|
+  #puts "This is loop number #{counter}"
+  #puts "Stop hitting yourself #{brother}!"
+  #counter += 1
+#end
